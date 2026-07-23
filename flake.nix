283edcfca86d85b4b0f3bd53d6dbec1a7859bddb
Leonardo Sá <leo@leo-sa.com>
2026-07-23 17:51:15 -0500

    Update vendorHash for go module changes

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
           version = "0.1.0";
           src = ./.;
 
-          vendorHash = "sha256-x2t89aXdJsvxtYm/18O8QnBqlPG2bcyA78PCUazAXOc=";
+          vendorHash = "sha256-cdSnpe2qwsvnRWwlrK9VXy9s4GSVOdN8lM2pVD1dZGo=";
 
           nativeBuildInputs = [ pkgs.go ];
 
